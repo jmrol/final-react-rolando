22693b6042357520187f89091ac9b71d67f8d7c6
Juan Mauel Rolando Pilia <juanmanuelrolando@gmail.com>
2024-03-20 19:08:55 -0300

    test coneccion

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -1,47 +1,47 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { CartContext } from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
 import CartItem from '../CartItem/CartItem';
 
 const Cart = () => {
 
-    const {cart,vaciarCarrito,eliminarItem,totalCarrito} = useContext(CartContext)
+    const { cart, vaciarCarrito, eliminarItem, totalCarrito } = useContext(CartContext)
 
     return (
         <div>
 
-            {cart.length == 0 
-            ? 
-            <>
-            <h1>NO HAY PRODUCTOS EN EL CARRITO</h1>
-            <Link to={"/"}>IR AL INICIO</Link>
-            </>
-                
+            {cart.length == 0
+                ?
+                <>
+                    <h1>NO HAY PRODUCTOS EN EL CARRITO</h1>
+                    <Link to={"/"}>IR AL INICIO</Link>
+                </>
+
                 :
 
                 <>
 
-                <h1>lista de carrito</h1>
+                    <h1>lista de carrito</h1>
 
-                {cart.map((p)=>(
-                    <CartItem key={p.producto.id} producto={p} eliminarItem={eliminarItem}/>
-                ))}
-                
+                    {cart.map((p) => (
+                        <CartItem key={p.producto.id} producto={p} eliminarItem={eliminarItem} />
+                    ))}
 
-                <p>Total: ${totalCarrito()}</p>
 
-                <button onClick={vaciarCarrito}>Vaciar carrito</button>
+                    <p>Total: ${totalCarrito()}</p>
 
-                <Link to={"/checkout"}>
-                    Completar compra
-                </Link>
+                    <button onClick={vaciarCarrito}>Vaciar carrito</button>
+
+                    <Link to={"/checkout"}>
+                        Completar compra
+                    </Link>
 
 
                 </>
 
-                
 
-            } 
+
+            }
         </div>
     );
 };
